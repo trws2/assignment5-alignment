b720e646aa1934adcd1e0a25364f65141afab370
trws2 <dchen@cs.umd.edu>
2025-08-07 23:08:00 -0700

    create and start implementing math_baseline.py

---
+++ cs336_alignment/math_baseline.py
@@ -0,0 +1,27 @@
+# imports
+import os
+import argparse
+import json
+from cs336_alignment.drgrpo_grader import r1_zero_reward_fn
+from vllm import LLM
+from cs336_alignment.vllm import evaluate_llm
+
+
+# define constants
+
+
+
+
+# main function that loads the examples and prompts and carry out the vllm evaluation
+
+
+
+
+
+# cli rountine
+
+
+
+
+
+
